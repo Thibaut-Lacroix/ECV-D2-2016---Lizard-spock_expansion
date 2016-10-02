30154356ac3bd03b4a6894a105572075411a3355
Thibaut-Lacroix <thibautlac@gmail.com>
2016-10-02 15:18:11 +0200

    transfer on github

---
+++ script.js
@@ -0,0 +1,89 @@
+// Cache selectors
+var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }
+});
+
+
+//INDICATORS
+;(function(window) {
+
+	'use strict';
+
+	function init() {
+		[].slice.call(document.querySelectorAll('.nav')).forEach(function(nav) {
+			var navItems = [].slice.call(nav.querySelectorAll('.nav__item')),
+				itemsTotal = navItems.length,
+				setCurrent = function(item) {
+					// return if already current
+					if( item.classList.contains('nav__item--current') ) {
+						return false;
+					}
+					// remove current
+					var currentItem = nav.querySelector('.nav__item--current');
+					currentItem.classList.remove('nav__item--current');
+
+					// set current
+					item.classList.add('nav__item--current');
+				};
+
+			navItems.forEach(function(item) {
+				item.addEventListener('click', function() { setCurrent(item); });
+			});
+		});
+
+		[].slice.call(document.querySelectorAll('.link-copy')).forEach(function(link) {
+			link.setAttribute('data-clipboard-text', location.protocol + '//' + location.host + location.pathname + '#' + link.parentNode.id);
+			new Clipboard(link);
+			link.addEventListener('click', function() {
+				link.classList.add('link-copy--animate');
+				setTimeout(function() {
+					link.classList.remove('link-copy--animate');
+				}, 300);
+			});
+		});
+	}
+
+	init();
+
+})(window);
